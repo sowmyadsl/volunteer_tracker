ebc0bf4c36bc64af16251306c2f5d058ac129885
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-05 11:45:08 -0700

    Add test case to save the volunteers to the database and pass it

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -17,11 +17,17 @@ class Volunteer
       last_name = volunteer.fetch('last_name')
       joining_date = volunteer.fetch('joining_date')
       leaving_date = volunteer.fetch('leaving_date')
+      assigned_project_id = volunteer.fetch('assigned_project_id').to_i
+      volunteers.push(Volunteer.new({:first_name => first_name, :last_name => last_name, :joining_date => joining_date, :leaving_date => leaving_date, :assigned_project_id => assigned_project_id}))
     end
     volunteers
   end
 
   define_method(:==) do |another_volunteer|
     self.first_name.==(another_volunteer.first_name).& (self.last_name.==(another_volunteer.last_name)).&(self.joining_date.==(another_volunteer.joining_date)).&(self.leaving_date.==(another_volunteer.leaving_date)).&(self.assigned_project_id.==(another_volunteer.assigned_project_id))
+  end
+
+  define_method(:save) do
+    DB.exec("INSERT INTO volunteers (first_name, last_name, joining_date, leaving_date, assigned_project_id) VALUES ('#{@first_name}','#{@last_name}', '#{@joining_date}','#{@leaving_date}', #{@assigned_project_id});")
   end
 end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -25,31 +25,38 @@ describe(Volunteer) do
 
   describe("#joining_date") do
     it("lets you add a joining date to volunteer") do
-      test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => 2017-03-04, :leaving_date => 2018-04-05, :assigned_project_id => 1})
-      expect(test_volunteer.joining_date()).to(eq(2017-03-04))
+      test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => "2017-03-04", :leaving_date => "2018-04-05", :assigned_project_id => 1})
+      expect(test_volunteer.joining_date()).to(eq("2017-03-04"))
     end
   end
 
   describe("#leaving_date") do
     it("lets you add a leaving date to volunteer") do
-      test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => 2017-03-04, :leaving_date => 2018-04-05, :assigned_project_id => 1})
-      expect(test_volunteer.leaving_date()).to(eq(2018-04-05))
+      test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => "2017-03-04", :leaving_date => "2018-04-05", :assigned_project_id => 1})
+      expect(test_volunteer.leaving_date()).to(eq("2018-04-05"))
     end
   end
 
   describe("#assigned_project_id") do
     it("lets you set a project ID to volunteer") do
-      test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => 2017-03-04, :leaving_date => 2018-04-05, :assigned_project_id => 1})
+      test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => "2017-03-04", :leaving_date => "2018-04-05", :assigned_project_id => 1})
       expect(test_volunteer.assigned_project_id()).to(eq(1))
     end
   end
 
   describe("#==") do
     it("is the same volunteer if it has the same first name, last name, joining date, leaving date and assigned project ID") do
-      test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => 2017-03-04, :leaving_date => 2018-04-05, :assigned_project_id => 1})
-      test_volunteer2 = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => 2017-03-04, :leaving_date => 2018-04-05, :assigned_project_id => 1})
+      test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => "2017-03-04", :leaving_date => "2018-04-05", :assigned_project_id => 1})
+      test_volunteer2 = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => "2017-03-04", :leaving_date => "2018-04-05", :assigned_project_id => 1})
       expect(test_volunteer).to(eq(test_volunteer2))
     end
   end
 
+  describe("#save") do
+    it("it will save a volunteer to the database") do
+    test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => "2017-03-04", :leaving_date => "2018-04-05", :assigned_project_id => 1})
+    test_volunteer.save
+    expect(Volunteer.all).to(eq([test_volunteer]))
+    end
+  end
 end
