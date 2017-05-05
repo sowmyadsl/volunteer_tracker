94df0cb7a3d9f7f057768f1c3149058f4eb77fef
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-05 10:53:44 -0700

    Add test case to set volunteer's volunteer's assigned project ID and pass it

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -6,9 +6,9 @@ require 'pg'
 
 DB = PG.connect({:dbname => 'volunteer_tracker_test'})
 
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM volunteer *;")
-    DB.exec("DELETE FROM project *;")
-  end
-end
+# RSpec.configure do |config|
+#   config.after(:each) do
+#     DB.exec("DELETE FROM volunteer *;")
+#     DB.exec("DELETE FROM project *;")
+#   end
+# end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -10,30 +10,37 @@ describe(Volunteer) do
   end
 
   describe("#first_name") do
-    it("lets you give a first name to volunteer") do
+    it("lets you add a first name to volunteer") do
       test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => 2017-03-04, :leaving_date => 2018-04-05, :assigned_project_id => 1})
       expect(test_volunteer.first_name()).to(eq("Sowmya"))
     end
   end
 
   describe("#last_name") do
-    it("lets you give a last name to volunteer") do
+    it("lets you add a last name to volunteer") do
       test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => 2017-03-04, :leaving_date => 2018-04-05, :assigned_project_id => 1})
       expect(test_volunteer.last_name()).to(eq("Dinavahi"))
     end
   end
 
   describe("#joining_date") do
-    it("lets you give a joining date to volunteer") do
+    it("lets you add a joining date to volunteer") do
       test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => 2017-03-04, :leaving_date => 2018-04-05, :assigned_project_id => 1})
       expect(test_volunteer.joining_date()).to(eq(2017-03-04))
     end
   end
 
   describe("#leaving_date") do
-    it("lets you give a leaving date to volunteer") do
+    it("lets you add a leaving date to volunteer") do
       test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => 2017-03-04, :leaving_date => 2018-04-05, :assigned_project_id => 1})
       expect(test_volunteer.leaving_date()).to(eq(2018-04-05))
+    end
+  end
+
+  describe("#assigned_project_id") do
+    it("lets you set a project ID to volunteer") do
+      test_volunteer = Volunteer.new({:first_name => "Sowmya", :last_name =>"Dinavahi", :joining_date => 2017-03-04, :leaving_date => 2018-04-05, :assigned_project_id => 1})
+      expect(test_volunteer.assigned_project_id()).to(eq(1))
     end
   end
 
