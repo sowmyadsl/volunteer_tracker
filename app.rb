1c1e329194deeadbfc872de781bd53e3f1acf53f
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-07 11:02:53 -0700

    Still updating the app.rb and added functionality to add volunteers

--- app.rb
+++ app.rb
@@ -21,17 +21,19 @@ get('/projects') do
 erb(:projects)
 end
 
-post('/project') do
+post('/projects') do
   project_name = params.fetch("proj_name")
   begin_date = params.fetch("begin_date")
   end_date = params.fetch("end_date")
   project = Project.new({:proj_name => project_name, :begin_date => begin_date, :end_date => end_date, :id => nil})
   project.save
-  erb(:project)
+  @projects = Project.all
+  erb(:projects)
 end
 
 get('/volunteers') do
   @volunteers = Volunteer.all
+  @projects = Project.all
   erb(:volunteers)
 end
 
@@ -40,7 +42,10 @@ post('/volunteers') do
   last_name = params.fetch("last_name")
   joining_date = params.fetch("joining_date")
   leaving_date = params.fetch("leaving_date")
-  volunteer = Volunteer.new({:first_name => first_name, :last_name => last_name, :joining_date => joining_date, :leaving_date => leaving_date})
+  assigned_project_id = params.fetch("assigned_project_id")
+  volunteer = Volunteer.new({:first_name => first_name, :last_name => last_name, :joining_date => joining_date, :leaving_date => leaving_date, :assigned_project_id => assigned_project_id})
   volunteer.save
+  @projects = Project.all
+  @volunteers = Volunteer.all
   erb(:volunteers)
 end
