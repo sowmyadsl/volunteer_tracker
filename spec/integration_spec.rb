b520e3a78e90160dbfd7ab0d4db2bff0a02c64e8
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-10 13:03:35 -0700

    Add integration specs

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -6,11 +6,17 @@ set(:show_exceptions, false)
 
 
 describe("the volunteer tracker path", {:type => :feature}) do
-  it("processes the user input and returns correct message if its a palindrome") do
+  it("returns a table of projects added on the first page") do
     visit("/")
-    fill_in("phrase1", :with => "madam")
-    fill_in("phrase2", :with => "anagram")
-    click_button("what am i?")
-    expect(page).to have_content("'madam' is a palindrome")
+    expect(page).to have_content("Welcome to the Volunteer Tracker There are no projects added yet! View All Projects View All Volunteers")
+  end
+
+  it("adds a project to database") do
+    visit("/projects")
+    fill_in("proj_name",:with => "humana")
+    fill_in("begin_date",:with => "17/03/02")
+    fill_in("end_date",:with => "18/03/04")
+    click_button("")
+    expect(page).to have_content("humana 17/03/02 18/03/04")
   end
 end
